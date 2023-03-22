7630724525e8bf2d574a3b87acdc6195075da80e
DOCOMO\5140801 <takanori.nagahara.ts@nttdocomo.com>
2023-03-22 16:43:27 +0900

    First commit Co-authored-by: 4151442+nagahar@users.noreply.github.com Co-authored-by: 22491305+Keito-Sasaoka@users.noreply.github.com

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,30 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+function VendingMachine() {
+  return {
+    till: { 10: 0, 50: 0, 100: 0, 500: 0 },
+    insertCoin(num) {
+      let a = Math.floor(num / 500);
+      let r = num % 500;
+      let b = Math.floor(r / 100);
+      let s = r % 100;
+      let c = Math.floor(s / 50);
+      let t = s % 50;
+      let d = Math.floor(t / 10);
+      this.till[500] += a;
+      this.till[100] += b;
+      this.till[50] += c;
+      this.till[10] += d;
+      this.balance =
+        500 * this.till[500] +
+        100 * this.till[100] +
+        50 * this.till[50] +
+        10 * this.till[10];
+    },
+    balance: 0,
+  };
+}
+
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -2,11 +2,14 @@ const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
+  // it("should be there", () => {
+  //   expect(VendingMachine).isDefined();
+  //   expect(typeof VendingMachine).toBe("function");
+  // });
+
   it("should accept valid coins", () => {
     // Setup
-    const machine = new VendingMachine();
-
-    // Exercise
+    const machine = VendingMachine();
     machine.insertCoin(500);
 
     // Assert
@@ -17,5 +20,16 @@ describe("vending machine", () => {
       500: 1,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
+
+    machine.insertCoin(660);
+
+    // Assert
+    expect(machine.till).to.deep.equal({
+      10: 1,
+      50: 1,
+      100: 1,
+      500: 2,
+    });
+    expect(machine.balance).to.equal(1160); // Use an ES6 getter
   });
 });
